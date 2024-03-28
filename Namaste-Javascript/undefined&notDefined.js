579b152eb5038ac5bfd93259fa003228658a2a01
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-03-28 22:02:02 +0530

    Create undefined&notDefined.js

---
+++ Namaste-Javascript/undefined&notDefined.js
@@ -0,0 +1,8 @@
+console.log(a); //undefined
+var a = 7;
+//Even before the above line is executed, js put value as undefined for a.
+
+console.log(a); //7
+
+// console.log(x);
+//We will get not defined error as we havent kept any memory for this.
